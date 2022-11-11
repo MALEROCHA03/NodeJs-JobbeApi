67f6be8ca995d8ea7b91f9aaf022ab189b8a16d0
Marìa Alejandra Rocha Ortega <87345177+MALEROCHA03@users.noreply.github.com>
2022-11-11 16:37:00 -0500

    feat(db): Add MongoDb

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 
+const dotenv = require('dotenv');
 
-const dotenv = require('dotenv')
 
-dotenv.config({path: './config/.env'})
+dotenv.config({ path: './config.env' });
 
+const connectDatabase = require('./config/database');
+
+
+connectDatabase();
+
+
+app.use(express.json());
 
 const PORT = process.env.PORT;
+
+
+const jobs = require('./routes/jobs');
+
+app.use('/api/v1', jobs);
+
+
 app.listen(PORT, () => {
-    console.log(`server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
-});
+    console.log(`Server started on port ${PORT} in ${process.env.NODE_ENV} mode.`);
+})
