3f68176b177bd72413a54f3e09ba47a90556142f
Marìa Alejandra Rocha Ortega <87345177+MALEROCHA03@users.noreply.github.com>
2022-11-16 10:10:14 -0500

    feat: authentication and authorization

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
-const fastify = require('fastify')(
-    {
-        logger: true
-    }
-);
+const fastify = require('fastify')();
 require('./utils/mongoose')
+fastify.register(require('@fastify/jwt'), {
+    secret: 'mysecret'
+})
+
+
 const routes = require('./routes/jobs.routes');
 
 const jobsRoutes = require("./routes/jobs.routes")
-fastify.get('/', (request, reply) => {
-    reply.send({hello: 'world'})
+
+fastify.decorate("authenticate", async function (request, reply) {
+    try {
+        await request.jwtVerify()
+    } catch (error) {
+        reply.send(error);
+    }
 });
 
+fastify.get('/generateToken/:id', (request, reply) => {
+    const data = {
+        name: request.params.id
+    }
+    const token = fastify.jwt.sign(data);
+    reply.send({ token });
+});
+
+fastify.get('/validateToken',
+    {
+    onRequest: [fastify.authenticate]
+    }, async function (request, reply) {
+        return request.user;
+    })
+
 jobsRoutes.forEach((routes) => {
-  fastify.route(routes);
+    fastify.route(routes);
 })
 
 const start = async () => {
